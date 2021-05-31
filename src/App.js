66e3c3b2331064ff92a918cb65733b58f1930878
vvvkeshav3 <57211890+vvvkeshav3@users.noreply.github.com>
2021-05-31 13:53:36 +0530

    Added useEffect to maintain loginState on Reload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLoginState = localStorage.getItem('isLoggedIn');
+    if (storedLoginState === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // Check for email and password
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
